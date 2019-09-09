13103df9ec3f7a020bbed91cc250b2a28920f1dc
Kidany <kidobv@gmail.com>
2019-09-09 19:40:58 -0400

    removed json DB - used for CRUD testing

--- client/src/apis/pagame.js
+++ client/src/apis/pagame.js
@@ -1,8 +1,5 @@
 import axios from 'axios'
-//3001 port for json-server 
-//3000 port for NodeJS server with MongoDB atlas
 export default axios.create({
-   // baseURL: 'http://54.173.140.184:3000',
     baseURL: 'http://localhost:3000',
     headers: {
         'Content-Type': 'application/json'
